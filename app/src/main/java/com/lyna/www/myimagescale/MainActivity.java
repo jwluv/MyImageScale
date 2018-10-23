13779d152fcdc7696ae2292932e1570747c0d45c
EDU <jwluv1220@gmail.com>
2018-10-23 13:29:59 +0900

    Initial commit

---
+++ app/src/main/java/com/lyna/www/myimagescale/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lyna.www.myimagescale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyImageView(this));
+    }
+}
